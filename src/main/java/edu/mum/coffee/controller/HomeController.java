bec362b438c10b0f14d173552e322b2e2caf4b5f
schuneal <sunilpaudel95@gmail.com>
2018-06-09 00:32:53 -0500

    home controller

--- src/main/java/edu/mum/coffee/controller/HomeController.java
+++ src/main/java/edu/mum/coffee/controller/HomeController.java
@@ -4,7 +4,6 @@ import java.util.Collection;
 
 import javax.annotation.Resource;
 
-import org.dom4j.util.UserDataAttribute;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
@@ -17,35 +16,35 @@ import edu.mum.coffee.service.PersonService;
 
 @Controller
 public class HomeController {
-	@Resource
-	private PersonService personService;
+    @Resource
+    private PersonService personService;
 
-	@GetMapping({ "/", "/index", "/home" })
-	public String homePage() {
-		return "home";
-	}
+    @GetMapping({ "/", "/index", "/home" })
+    public String homePage() {
+        return "home";
+    }
 
-	@GetMapping({ "/secure" })
-	public String securePage(@AuthenticationPrincipal final UserDetails userdetails, Model model) {
-		
-		// get user name here ...
-		String username = userdetails.getUsername();
-		Collection<? extends GrantedAuthority> roles = userdetails.getAuthorities();
-		GrantedAuthority arole = roles.stream().findFirst().get();
-		String role = arole.toString();
-		System.out.println("Role: "+role);
-		System.out.println("Username: "+username);
-		
-		
-		//Get person object by email
-		Person person = personService.findByEmail(username).get(0);
-		model.addAttribute("person", person);
-		System.out.println("First Name: "+person.getFirstName());
-				
-		if (role.equals("ROLE_USER")) {
-			return "user-page";
-		} else {
-			return "admin-page";
-		}
-	}
+    @GetMapping({ "/secure" })
+    public String securePage(@AuthenticationPrincipal final UserDetails userdetails, Model model) {
+
+        // get user name here ...
+        String username = userdetails.getUsername();
+        Collection<? extends GrantedAuthority> roles = userdetails.getAuthorities();
+        GrantedAuthority arole = roles.stream().findFirst().get();
+        String role = arole.toString();
+        System.out.println("Role: "+role);
+        System.out.println("Username: "+username);
+
+
+        //Get person object by email
+        Person person = personService.findByEmail(username).get(0);
+        model.addAttribute("person", person);
+        System.out.println("First Name: "+person.getFirstName());
+
+        if (role.equals("ROLE_USER")) {
+            return "user-page";
+        } else {
+            return "admin-page";
+        }
+    }
 }
