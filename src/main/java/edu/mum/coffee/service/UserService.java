868308342d50fa4bb70618aa94ec1976d3c3ca39
schuneal <sunilpaudel95@gmail.com>
2018-06-09 00:36:46 -0500

    user service

---
+++ src/main/java/edu/mum/coffee/service/UserService.java
@@ -0,0 +1,32 @@
+package edu.mum.coffee.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import edu.mum.coffee.domain.Person;
+import edu.mum.coffee.domain.User;
+import edu.mum.coffee.repository.PersonRepository;
+import edu.mum.coffee.repository.UserRepository;
+
+@Service
+@Transactional
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	public User saveUser(User user) {
+		return userRepository.save(user);
+	}
+	public User findByUsername(String username) {
+		return userRepository.findByUsername(username);
+	}
+
+	public void removeUser(User user) {
+		userRepository.delete(user);
+	}
+
+}
