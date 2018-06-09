14560f46fd1080321c3c294bcc38309f24169d59
schuneal <sunilpaudel95@gmail.com>
2018-06-09 00:33:39 -0500

    user

--- src/main/java/edu/mum/coffee/domain/User.java
+++ src/main/java/edu/mum/coffee/domain/User.java
@@ -6,12 +6,59 @@ import javax.persistence.Id;
 
 @Entity
 public class User {
-	
+
 	@Id
 	@GeneratedValue
 	private int id;
-	private int username;
-	private int password;
-	private int role;
+	private String username;
+	private String password;
+	private String role;
+
+	public User() {
+
+	}
+
+	public User(String username, String password, String role) {
+
+		this.username = username;
+		this.password = password;
+		this.role = role;
+	}
+
+	public User(User user) {
+		this.username = user.username;
+		this.password = user.password;
+		this.role = user.role;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
 
 }
