6e76d3bdb9485f803f4650fefeecdb4a8270620e
schuneal <sunilpaudel95@gmail.com>
2018-06-10 01:27:52 -0500

    security config

--- src/main/java/edu/mum/coffee/config/WebSecurityConfig.java
+++ src/main/java/edu/mum/coffee/config/WebSecurityConfig.java
@@ -7,10 +7,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
 import edu.mum.coffee.repository.UserRepository;
 import edu.mum.coffee.service.MyUserDetailService;
 
@@ -24,8 +21,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		/*http.sessionManagement()
- 			.sessionCreationPolicy(SessionCreationPolicy.STATELESS);*/
+
 		http
 				.authorizeRequests()
 				.antMatchers("/", "/home", "/index").permitAll()
@@ -46,24 +42,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 
-		auth.userDetailsService(userDetailsService)
-				.passwordEncoder(new PasswordEncoder() {
+		auth.userDetailsService(userDetailsService);
 
-					@Override
-					public String encode(CharSequence charseq) {
-						// TODO Auto-generated method stub
-						return charseq.toString();
-					}
-
-					@Override
-					public boolean matches(CharSequence arg0, String arg1) {
-						// TODO Auto-generated method stub
-						return true;
-					}
-
-				});
-
-
-//		auth.inMemoryAuthentication().withUser("akassa@mum.edu").password("pw").roles("ADMIN");
 	}
 }
